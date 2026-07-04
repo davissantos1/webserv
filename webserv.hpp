dd4bc1268bbd744f8b3864b947fed983f1d70ee8
Davi Santos <davisimoesdossantos1@gmail.com>
2026-07-04 15:58:48 -0300

    feat: main webserv.hpp included

---
+++ webserv.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/28 16:33:48 by dasimoes          #+#    #+#             */
+/*   Updated: 2026/07/04 15:47:03 by dasimoes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef WEBSERV_HPP
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include <map>
+#include <vector>
+
+#endif
