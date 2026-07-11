581c327f74969452217b6b64c4d0961e3b01b887
vitosant <vitor.200483@gmail.com>
2026-07-11 17:06:43 -0300

    feat(config parser): start tokenizer struct

--- include/ConfigParser.hpp
+++ include/ConfigParser.hpp
@@ -3,25 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   ConfigParser.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 08:36:47 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 08:56:23 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/07/11 12:15:40 by vitosant         ###    ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIGPARSER_HPP
 # define CONFIGPARSER_HPP
 
+#include "Location.hpp"
+#include "VirtualHostConfig.hpp"
+#include <cstddef>
+#include <fstream>
+#include <deque>
+#include <string>
+#include <utility>
+#include <vector>
+
+typedef enum e_tokens
+{
+	TOKEN_L_BRACE,
+	TOKEN_R_BRACE,
+	TOKEN_HASH,
+	TOKEN_SEMICOLON,
+	TOKEN_NEWLINE,
+	TOKEN_WORD
+}	t_file_tokens;
+
 class ConfigParser
 {
 	private:
-		// to be added Vitor
+		std::deque< std::pair<t_file_tokens, std::string> >	_tokens;
+		std::string											_filePath;
+		std::size_t											_pos;
+
+		void				makeTokens( std::ifstream& file );
+		bool				analyseTokens( void );
+
+		Location			parseLocation( void );
+		VirtualHostConfig	parseVirtHost( void );
+		void				mountConfigVec( std::vector<VirtualHostConfig> & configs );
+
 	public:
-		ConfigParser();
-		~ConfigParser();
+		ConfigParser( void );
+		~ConfigParser( void );
 		ConfigParser(const ConfigParser& other);
 		ConfigParser& operator=(const ConfigParser& other);
+
+		std::vector<VirtualHostConfig> parse( const std::string& path );
 };
 
 #endif
--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -11,30 +11,77 @@
 /* ************************************************************************** */
 
 #include "ConfigParser.hpp"
+#include "VirtualHostConfig.hpp"
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
 
-ConfigParser::ConfigParser()
-{
+ConfigParser::ConfigParser( void ) {}
 
-}
+ConfigParser::~ConfigParser( void ) {}
 
-ConfigParser::~ConfigParser()
-{
-
-}
-
-ConfigParser::ConfigParser(const ConfigParser& other)
-{
-	if (this != &other)
-		*this = other;
-}
+ConfigParser::ConfigParser(const ConfigParser& other): _tokens(other._tokens), _filePath(other._filePath), _pos(other._pos){}
 
 ConfigParser&	ConfigParser::operator=(const ConfigParser& other)
 {
-	if (this != other)
+	if (this != &other)
 	{
-		// whatever
+		_tokens = other._tokens;
+		_pos = other._pos;
+		_filePath = other._filePath;
 	}
 	return (*this);
 }
 
-// to be added Vitor
+std::vector<VirtualHostConfig> ConfigParser::parse( const std::string &path )
+{
+	std::vector<VirtualHostConfig>	configs;
+	std::ifstream					file;
+
+	file.open(path.c_str());
+	if (file.is_open() == true)
+	{
+		makeTokens(file);
+		if (analyseTokens() == true)
+			mountConfigVec(configs);
+		file.close();
+	}
+	else
+		std::cerr << "Error: Couldn't read " << path << " file." << std::endl;
+	return (configs);
+}
+
+static std::pair<t_file_tokens, std::string> makePair( t_file_tokens type, std::string text )
+{
+	std::pair<t_file_tokens, std::string>	token;
+
+	token.first = type;
+	token.second = text;
+	return token;
+}
+
+void	ConfigParser::makeTokens( std::ifstream& file )
+{
+	std::string			line;
+	std::stringstream	ss;
+	std::string			token_text;
+
+	while (!file.eof())
+	{
+		getline(file, line);
+
+		ss.str("");
+		ss << line;
+		getline(ss, token_text, ' ');
+
+		if (token_text.find('#') != std::string::npos)
+			std::cout << "tratar # no meio da linha --nao_esquecer" << std::endl;
+
+		if (token_text.length() > 1)
+			{}
+	}
+}
