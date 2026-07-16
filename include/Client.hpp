6c4b8beb41e646098f61b00b620c9a48591d531b
Davi Santos <davisimoesdossantos1@gmail.com>
2026-07-16 04:05:06 -0300

    feat: modeled Client class' hpp and implemented HTTP request method

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 23:45:07 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 08:50:58 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/07/15 17:11:57 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,47 @@
 # define CLIENT_HPP
 
 #include "webserv.hpp"
+#include <string>
+#include <iostream>
 
-enum Status;
+class HttpRequest;
+class HttpResponse;
+
+enum ClientStatus;
 {
-	REQUEST_PROCESSING,
-	RESPONSE_PROCESSING,
-	CGI_PROCESSING,
-	REQUEST_READY,
-	RESPONSE_READY,
-	CGI_READY
+	READING_REQUEST,
+	EXECUTING_CGI,
+	PREPARING_RESPONSE,
+	WRITING_RESPONSE,
+	DISCONNECT
 };
 
 class Client
 {
 	private:
-		enum Status;
-		int	clientFd;
+		std::string			_ip;
+		uint16_t			_port;
+		int					_fd;
+		enum ClientStatus	_status;
+		HttpRequestParser	_requestParser;
+		HttpResponseBuilder	_requestBuilder;
+		time_t				_lastActivity;
 	public:
 		Client();
 		~Client();
 		Client(const Client& other);
+		Client(std::string ip, uint16_t port, int fd);
 		Client&	operator=(const Client& other);
+		void	processHttpRequest(int fd);
+		void	processHttpResponse(int fd);
+		class	ClientException: std::exception
+		{
+			private:
+				int	_errno;
+			public:
+				ClientException(int err): _errno(err) {}
+				const char* what() const throw { return std::strerror(_errno); }
+		};	
 };
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -6,21 +6,18 @@
 /*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 00:30:39 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 09:04:53 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/07/15 17:35:47 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 
-Client::Client()
-{
+Client::Client() {}
 
-}
+Client::Client(std::string ip, uint16_t port, int fd): _ip(ip), _port(port), _fd(fd) {}
 
 Client::~Client()
-{
-
-}
+{}
 
 Client::Client(const Client& other)
 {
@@ -32,7 +29,42 @@ Client&	Client::operator=(const Client& other)
 {
 	if (this != other)
 	{
-		// whatever
+		this->_fd = other._fd;
+		this->_ip = other._ip;
+		this->_port = other._port;
+		this->_status = other._status;
+		this->_request = other._request;
+		this->_response = other._response;
+		this->_responseBuilder = other._responseBuilder;
+		this->_requestParser = other._requestParser;
+		this->_lastActivity = other._lastActivity;
 	}
 	return (*this);
+}
+
+int	Client::processHttpRequest(int fd)
+{
+	char tempBuffer[8192];
+
+	while (true)
+	{
+		ernno = 0;
+		ssize_t bytes = recv(fd, tempBuffer, sizeof(tempBuffer));
+		if (bytes > 0)
+			this->_httpRequestParser.feed(tempBuffer);
+		else if (bytes == 0)
+			return (-1);
+		else
+		{
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+				break
+			throw (ClientException(errno));
+		}
+	}
+	return (0);
+}
+
+void	Client::processHttpResponse(int fd)
+{
+
 }
