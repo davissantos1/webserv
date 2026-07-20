da9201c4ef6d615551adad5f4f5532ce1b746465
vitosant <vitor.200483@gmail.com>
2026-07-19 21:11:57 -0300

    feat(setter-adder): add setters and adders to classes VirtualHostConfig and Location

--- include/Location.hpp
+++ include/Location.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Location.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 17:29:09 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 08:57:07 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/07/19 17:10:55 by vitosant         ###    ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,15 @@ class	Location
 		~Location();
 		Location(const Location& other);
 		Location&	operator=(const Location& other);
+
+		void	setRoot( const std::string& root ) { _root = root; }
+		void	setIndex( const std::vector<std::string> & index ) { _index = index; }
+		void	setPaths( const std::vector<std::string> & paths ) { _paths = paths; }
+		void	setAllowedMethods( const std::vector<std::string> & allowedMethods ) { _allowedMethods = allowedMethods; }
+
+		void	addIndex( const std::string& index ) { _index.push_back(index); }
+		void	addPath( const std::string& path ) { _allowedMethods.push_back(path); }
+		void	addAllowedMethods( const std::string& method ) { _allowedMethods.push_back(method); }
 };
 
 #endif
--- include/VirtualHostConfig.hpp
+++ include/VirtualHostConfig.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   VirtualHostConfig.hpp                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 15:37:28 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 08:50:59 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/07/19 17:10:42 by vitosant         ###    ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ class VirtualHostConfig
 {
 	private:
 		std::vector<std::string>	_serverNames;
-		std::string					_hostIp;
-		int							_port;
+		std::vector<std::string>	_hostIp;
+		std::vector<int>			_port;
 		std::map<int, std::string>	_errorPages;
 		std::size_t					_maxBodySize;
 		std::string					_root;
@@ -39,14 +39,34 @@ class VirtualHostConfig
 		VirtualHostConfig&	operator=(const VirtualHostConfig& other);
 
 		const std::vector<std::string>& getServerNames() const { return _serverNames; }
-		const std::string& getHostIp() const { return _hostIp; }
-		int getPort() const { return _port; }
+		const std::vector<std::string> & getVecHostIp() const { return _hostIp; }
+		const std::vector<int> & getVecPort() const { return _port; }
+		const std::string& getHostIp( const int i ) const { return (_hostIp[i]); }
+		const int getPort( const int i ) const { return (_port[i]); }
 		const std::map<int, std::string>& getErrorPages() const { return _errorPages; }
 		std::size_t getMaxBodySize() const { return _maxBodySize; }
 		const std::string& getRoot() const { return _root; }
 		const std::vector<std::string>& getIndex() const { return _index; }
 		const std::vector<std::string>& getAllowedMethods() const { return _allowedMethods; }
 		const std::vector<Location>& getLocation() const { return _locations; }
+
+		void	setServerNames( const std::vector<std::string> & serverNames ) { _serverNames = serverNames; }
+		void	setHostIp( const std::vector<std::string> & hostIp ) { _hostIp = hostIp; }
+		void	setPort( const std::vector<int> & port ) { _port = port; }
+		void	setErrorPages( const std::map<int, std::string> & errorPages ) { _errorPages = errorPages; }
+		void	setMaxBodySize( const std::size_t maxBodySize ) { _maxBodySize = maxBodySize; }
+		void	setRoot( const std::string& root ) { _root = root; }
+		void	setIndex( const std::vector<std::string> & index ) { _index = index; }
+		void	setAllowedMethods( const std::vector<std::string> & allowedMethods ) { _allowedMethods = allowedMethods; }
+		void	setLocations( const std::vector<Location> & locations ) { _locations = locations; }
+
+		void	addServerName( const std::string& name ) { _serverNames.push_back(name); }
+		void	addHostIp( const std::string& hostIp ) { _hostIp.push_back(hostIp); }
+		void	addPort( const int port ) { _port.push_back(port); }
+		void	addErrorPage( const int errorCode, const std::string& errorPath ) { _errorPages[errorCode] = errorPath; }
+		void	addIndex( const std::string& index ) { _index.push_back(index); }
+		void	addAllowedMethod( const std::string& method ) { _allowedMethods.push_back(method); }
+		void	addLocation( const Location& location ) { _locations.push_back(location); }
 };
 
 #endif
