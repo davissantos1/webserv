029e06e3c9e38f407f831419e613f954271e6ae0
vitosant <vitor.200483@gmail.com>
2026-07-24 17:05:07 -0300

    refact: add new atributes at Location class to handle cgi

--- include/Location.hpp
+++ include/Location.hpp
@@ -6,7 +6,7 @@
 /*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 17:29:09 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/19 17:10:55 by vitosant         ###    ########.fr      */
+/*   Updated: 2026/07/24 14:25:34 by vitosant         ###    ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,53 @@
 #include <string>
 #include <vector>
 
-class	Location
+class Location
 {
 	private:
+		std::string					_path;
 		std::string					_root;
 		std::vector<std::string>	_index;
-		std::vector<std::string>	_paths;
 		std::vector<std::string>	_allowedMethods;
-	public:
-		Location();
-		~Location();
-		Location(const Location& other);
-		Location&	operator=(const Location& other);
 
+		bool						_autoindex;
+
+		bool						_uploadEnable;
+		std::string					_uploadStore;
+
+		std::string					_cgiExtension;
+		std::string					_cgiPath;
+
+	public:
+		Location( void );
+		~Location( void );
+		Location( const Location& other );
+		Location&	operator=( const Location& other );
+
+		void	setPath( const std::string& path ) { _path = path; }
 		void	setRoot( const std::string& root ) { _root = root; }
 		void	setIndex( const std::vector<std::string> & index ) { _index = index; }
-		void	setPaths( const std::vector<std::string> & paths ) { _paths = paths; }
 		void	setAllowedMethods( const std::vector<std::string> & allowedMethods ) { _allowedMethods = allowedMethods; }
+		void	setAutoindex( bool autoindex ) { _autoindex = autoindex; }
+		void	setUploadEnable( bool uploadEnable ) { _uploadEnable = uploadEnable; }
+		void	setUploadStore( const std::string& uploadStore ) { _uploadStore = uploadStore; }
+		void	setCgiExtension( const std::string& cgiExtension ) { _cgiExtension = cgiExtension; }
+		void	setCgiPath( const std::string& cgiPath ) { _cgiPath = cgiPath; }
 
 		void	addIndex( const std::string& index ) { _index.push_back(index); }
-		void	addPath( const std::string& path ) { _allowedMethods.push_back(path); }
-		void	addAllowedMethods( const std::string& method ) { _allowedMethods.push_back(method); }
+		void	addAllowedMethod( const std::string& method ) { _allowedMethods.push_back(method); }
+
+		const std::string&					getPath( void ) const { return _path; }
+		const std::string&					getRoot( void ) const { return _root; }
+		const std::vector<std::string> &	getIndex( void ) const { return _index; }
+		const std::vector<std::string> &	getAllowedMethods( void ) const { return _allowedMethods; }
+
+		bool								getAutoindex( void ) const { return _autoindex; }
+
+		bool								getUploadEnable( void ) const { return _uploadEnable; }
+		const std::string&					getUploadStore( void ) const { return _uploadStore; }
+
+		const std::string&					getCgiExtension( void ) const { return _cgiExtension; }
+		const std::string&					getCgiPath( void ) const { return _cgiPath; }
 };
 
 #endif
--- src/Location.cpp
+++ src/Location.cpp
@@ -3,35 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   Location.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 17:59:43 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 09:08:33 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/07/24 14:31:04 by vitosant         ###    ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Location.hpp"
 
-Location::Location() {}
+Location::Location( void ): _autoindex(false), _uploadEnable(false) {}
 
-Location::~Location() {}
+Location::~Location( void ) {}
 
-Location::Location(const Location& other)
+Location::Location( const Location& other )
 {
 	if (this != &other)
 		*this = other;
 }
 
-Location&	Location::operator=(const Location& other)
+Location&	Location::operator=( const Location& other )
 {
 	if (this != &other)
 	{
 		this->_root = other._root;
 		this->_index = other._index;
-		this->_paths = other._paths;
+		this->_path = other._path;
 		this->_allowedMethods = other._allowedMethods;
+		this->_autoindex = other._autoindex;
+		this->_uploadEnable = other._uploadEnable;
+		this->_uploadStore = other._uploadStore;
+		this->_cgiExtension = other._cgiExtension;
+		this->_cgiPath = other._cgiPath;
 	}
 	return (*this);
 }
-
-// to be added Vitor
