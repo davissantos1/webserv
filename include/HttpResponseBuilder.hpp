d717039edb9342ef0e808fd389689eb926f8f3ec
Davi Santos <davisimoesdossantos1@gmail.com>
2026-07-30 22:16:07 -0300

    feat: started to implement builder functionality to assemble HTTP response and handle errors correctly

--- include/HttpResponseBuilder.hpp
+++ include/HttpResponseBuilder.hpp
@@ -6,22 +6,40 @@
 /*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 08:36:47 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 15:26:53 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/07/30 18:05:05 by davi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HTTPRESPONSEBUILDER_HPP
 # define HTTPRESPONSEBUILDER_HPP
 
+# include <string>
+
 class HttpResponseBuilder
 {
 	private:
-
+		int				_statusCode;
+		int				_httpBodySize;
+		int				_writtenBytes;
+		HttpResponse	_response;
+		std::string		_httpResponse;
+		std::string		_httpBody;
+		
 	public:
 		HttpResponseBuilder();
 		~HttpResponseBuilder();
 		HttpResponseBuilder(const HttpResponseBuilder& other);
 		HttpResponseBuilder& operator=(const HttpResponseBuilder& other);
+
+		int		getStatusCode() { return this->_statusCode; }
+		HttpResponse&	getResponse() { return this->_response; }
+		std::string&	getHttpResponse() { return this->_httpResponse; }
+
+		void			setStatusCode(int statusCode) { this->_statusCode = statusCode; }
+		
+		void	buildResponse();
+		void	feed(char *buffer, int size);
+		void	startBuilder(std::string httpBody, int httpBodySize);
 };
 
 #endif
