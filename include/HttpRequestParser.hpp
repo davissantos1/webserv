c05d3210982fa288cda73f1cc3ee52962f1d909d
vitosant <vitor.200483@gmail.com>
2026-08-09 14:41:25 -0300

    feat(request parser): start HttpRequestParser::feed() to parse requests

--- include/HttpRequestParser.hpp
+++ include/HttpRequestParser.hpp
@@ -14,34 +14,50 @@
 # define HTTPREQUESTPARSER_HPP
 
 // deal with the connection and the transfer-encoding header
-// This parser will be fed a bit at a time and have an internal state 
+// This parser will be fed a bit at a time and have an internal state
 // I need to know specific states from feed to send an error if the request is malformed or filled with errors
 // I'm assuming an enum for each state, tell me if you change it, please
-// This project is draining my soul 
+// This project is draining my soul
 
-# include <string>
+#include "HttpRequest.hpp"
+#include <cstddef>
 
-class HttpRequest;
+enum RequestStatus {
+	PARSING_REQUEST_LINE,
+	PARSING_HEADERS,
+	PARSING_BODY,
+	DONE,
+	ERROR
+};
 
 // To be added Vitor
 class HttpRequestParser
 {
 	private:
-		enum RequestStatus	_requestStatus;
-		HttpRequest			_httpRequest;
+		RequestStatus	_requestStatus;
+		HttpRequest		_httpRequest;
+		std::string		_buffer;
+		std::size_t		_expectedBodyLen;
+		bool			_chunked;
+		std::size_t		_pos;
+
+		void	handleRequestLine( const std::string& str );
+		void	handleHeaders( const std::string& str );
+		void	handleBody( const std::string& str );
+
 	public:
-		HttpRequestParser();
-		~HttpRequestParser();
-		HttpRequestParser(const HttpRequestParser& other);
-		HttpRequestParser& operator=(const HttpRequestParser& other);
+		HttpRequestParser( void );
+		~HttpRequestParser( void );
+		HttpRequestParser( const HttpRequestParser& other );
+		HttpRequestParser& operator=( const HttpRequestParser& other );
 
 		enum RequestStatus	getRequestStatus() { return this->_requestStatus; }
 		HttpRequest&		getHttpRequest() { return this->_httpRequest; }
 		HttpRequest*		getHttpRequestPointer() { return &this->_httpRequest; }
 
-		enum RequestStatus	feed(char* buffer, int size); // to be done
+		enum RequestStatus	feed( const char* buffer, size_t size ); // to be done
 		bool				hasCgi(); // to be done
-		
+
 };
 
 #endif
--- src/HttpRequestParser.cpp
+++ src/HttpRequestParser.cpp
@@ -3,24 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   HttpRequestParser.cpp                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 08:58:16 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/07/07 09:08:33 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/08/09 18:47:01 by vitosant         ###    ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HttpRequestParser.hpp"
+#include "HttpRequest.hpp"
+#include "string_utils.hpp"
+#include <cstddef>
+#include <string>
+#include <vector>
 
-HttpRequestParser::HttpRequestParser()
-{
+HttpRequestParser::HttpRequestParser( void ): _requestStatus(PARSING_REQUEST_LINE), _expectedBodyLen(0), _chunked(false), _pos(0) {}
 
-}
-
-HttpRequestParser::~HttpRequestParser()
-{
-
-}
+HttpRequestParser::~HttpRequestParser( void ) {}
 
 HttpRequestParser::HttpRequestParser(const HttpRequestParser& other)
 {
@@ -30,11 +29,77 @@ HttpRequestParser::HttpRequestParser(const HttpRequestParser& other)
 
 HttpRequestParser&	HttpRequestParser::operator=(const HttpRequestParser& other)
 {
-	if (this != other)
+	if (this != &other)
 	{
-		// whatever
+		_requestStatus = other._requestStatus;
+		_httpRequest = other._httpRequest;
+		_buffer = other._buffer;
+		_expectedBodyLen = other._expectedBodyLen;
+		_chunked = other._chunked;
+		_pos = other._pos;
 	}
 	return (*this);
 }
 
-// to be added Vitor
+enum RequestStatus HttpRequestParser::feed( const char* buffer, size_t len )
+{
+	std::string	tmp;
+	std::size_t	i;
+
+	_buffer.append(buffer, len);
+	while (_requestStatus != DONE && _requestStatus != ERROR)
+	{
+		i = _buffer.find("\r\n");
+		if (i == std::string::npos)
+			break ;
+		if (_requestStatus == PARSING_REQUEST_LINE || _requestStatus == PARSING_HEADERS)
+		{
+			std::string param = _buffer.substr(0, i);
+
+			if (_requestStatus == PARSING_REQUEST_LINE)
+				handleRequestLine(param);
+			else
+				handleHeaders(param);
+			_buffer.erase(0, i + 2);
+		}
+		else
+		{
+			handleBody(_buffer);
+		}
+	}
+	return (_requestStatus);
+}
+
+
+void	HttpRequestParser::handleRequestLine( const std::string& str )
+{
+	std::vector<std::string> splited = split(str, ' ');
+
+	if (splited.size() != 4 || (splited[0] != "GET" && splited[0] != "POST" && splited[0] != "DELETE"))
+	{
+		_requestStatus = ERROR;
+		return ;
+	}
+
+	_httpRequest.setMethod(splited[0]);
+	_httpRequest.setUri(splited[1]); // Validar aqui buscando ..
+	_httpRequest.setVersion(splited[2]);
+
+	if (splited[2] != "HTTP/1.1" && splited[2] != "HTTP/1.0")
+	{
+		_requestStatus = ERROR;
+		return ;
+	}
+
+	_requestStatus = PARSING_HEADERS;
+}
+
+void	HttpRequestParser::handleHeaders( const std::string& str )
+{
+	//essa aqui acho q vai ser trampo aqui
+}
+
+void	HttpRequestParser::handleBody( const std::string& str )
+{
+	// minha nossa como eu amo parsear, eu sou o maior parseador desse mundo, faço 2050 parseadas num sentada. aqui
+}
